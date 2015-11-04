7faa838a4a2caa4f7f4b00a86fbbe96076abaf77
Amal Antony <amalantony06@gmail.com>
2015-11-04 16:38:30 +0530

    Enable ES6 Babelify for the client side code

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var babel = require('gulp-babel');
 
 gulp.task('browserify', function() {
 	gulp.src('client/client.js')
 		.pipe(plumber())
-		.pipe(browserify({transform: 'reactify', debug: true}))
+		.pipe(browserify({debug: true}))
+		.pipe(babel({
+			presets: ['es2015', 'react']
+		}))
 		.pipe(concat('client.js'))
 		.pipe(gulp.dest('public'));
 });
