917e460c893f6b95b98176a0ba76b6ceb28a0965
Amal Antony <amalantony06@gmail.com>
2015-11-05 22:27:59 +0530

    Navigation compoent up

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -14,9 +14,23 @@ class Navigation extends React.Component{
 	}
 
 	render() {
-		return (<div>
-			<p>Navigation!</p>
-		</div>);
+		return (<nav className="navbar navbar-default">
+			<div className="container-fluid">
+				{/* Logo */}
+				<div className="navbar-header">
+					<a href="#" className="navbar-brand">MIVID Logo</a>
+				</div>
+
+				{/* Menu items */}
+				<div>
+					<ul className="nav navbar-nav">
+						<li className="active"> <a href="#"> Feed </a> </li>
+						<li> <a href="#"> Login </a> </li>
+						<li> <a href="#"> Signup </a> </li>
+					</ul>
+				</div>
+			</div>
+		</nav>);
 	}
 }
 
--- client/components/Skeleton.js
+++ client/components/Skeleton.js
@@ -5,11 +5,19 @@ const React = require('react');
 const RouteHandler = require('react-router').RouteHandler;
 const Navigation = require('./Navigation.js');
 
+class Container extends React.Component {
+	render() {
+		return (<div className="container">
+			<RouteHandler></RouteHandler>
+		</div>);
+	}
+}
+
 class Skeleton extends React.Component {
 	render() {
 		return(<div>
 			<Navigation></Navigation>
-			<RouteHandler></RouteHandler>
+			<Container></Container>
 		</div>);
 	}
 }
