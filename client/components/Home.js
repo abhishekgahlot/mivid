479c66403ee91a99288de9542a86a529b5bfa542
Amal Antony <amalantony06@gmail.com>
2015-11-07 15:54:39 +0530

    Added more thumbnails to test the view

--- client/components/Home.js
+++ client/components/Home.js
@@ -6,7 +6,14 @@ const VideoThumbnail = require('./VideoThumbnail');
 
 class Home extends React.Component {
 	render() {
-		return <VideoThumbnail/>;
+		return (<div class="row">
+			<VideoThumbnail></VideoThumbnail>
+			<VideoThumbnail></VideoThumbnail>
+			<VideoThumbnail></VideoThumbnail>
+			<VideoThumbnail></VideoThumbnail>
+			<VideoThumbnail></VideoThumbnail>
+			<VideoThumbnail></VideoThumbnail>
+		</div>);
 	}
 }
 
--- client/components/VideoThumbnail.js
+++ client/components/VideoThumbnail.js
@@ -3,21 +3,19 @@ const React = require('react');
 class VideoThumbnail extends React.Component {
 	render() {
 		return (
-			<div className="row">
-				<div className="col-xs-6 col-md-3">
-					<div href="#" className="thumbnail">
-						<a href="#">
-							<img
-								src="http://cdn.shopify.com/s/files/1/0224/1915/files/bunny.jpg?22110"
-								alt="..." />
-						</a>
-						<div className="caption">
-							<a href="#"> <p>Best Bunny video ever!</p> </a>
-							<p>
-								<span> Views (1257) </span>
-								<span className="pull-right"> Points (257) </span>
-							</p>
-						</div>
+			<div className="col-xs-6 col-md-3">
+				<div href="#" className="thumbnail">
+					<a href="#">
+						<img
+							src="http://cdn.shopify.com/s/files/1/0224/1915/files/bunny.jpg?22110"
+							alt="..." />
+					</a>
+					<div className="caption">
+						<a href="#"> <p>Best Bunny video ever!</p> </a>
+						<p>
+							<span> Views (1257) </span>
+							<span className="pull-right"> Points (257) </span>
+						</p>
 					</div>
 				</div>
 			</div>
