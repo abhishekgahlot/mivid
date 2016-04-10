3adc947a6c9cc5abba2524d18c0beb92b69ad6b6
AbhishekGahlot <me@abhishek.it>
2016-04-10 16:21:16 +0530

    Modified bootstrap

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,7 +28,6 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/less/dist/less.js',
       'bower_components/dropzone/dist/min/dropzone.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
